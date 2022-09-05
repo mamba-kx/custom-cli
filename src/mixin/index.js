99b68ff31d78aa755c512572092ce2ac552100b1
mamba-kx <1109046340@qq.com>
2022-09-05 10:24:44 +0800

    feat:增加混入mixin

---
+++ src/mixin/index.js
@@ -0,0 +1,10 @@
+export const mixin = {
+  data() {
+    return {
+      name: "ckx"
+    };
+  },
+  created() {
+    console.log("mixin");
+  }
+};
--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,16 +1,21 @@
 <template>
   <div class="home">
     <van-button type="info" block @click="toDemo">首页按钮</van-button>
+    {{ name }}
   </div>
 </template>
 
 <script>
+import { mixin } from '@/mixin';
 export default {
+  mixins: [mixin],
   components: {},
   data() {
     return {};
   },
-  created() { },
+  created() {
+    console.log('home');
+  },
   mounted() { },
   methods: {
     toDemo() {
